f045313fd047d301c921ed83112922918fe28d95
wang <passionymph@163.com>
2018-03-02 16:53:07 +0800

    defined ship's fields name.
    (cherry picked from commit 9a0dfa0bf5ea2591c413e0d8dfa9d338883be9c7)

--- history.h
+++ history.h
@@ -6,14 +6,19 @@
 #define HELLO_HISTORY_BASE_H
 
 #include <boost/date_time/gregorian/gregorian.hpp>
-using namespace boost::gregorian;
+//using namespace boost::gregorian;
+using boost::gregorian::date;
 
 #include <boost/variant.hpp>
 using boost::variant;
 
 #include <unordered_map>
 #include <utility>
-using namespace std;
+//using namespace std;
+using std::string;
+using std::vector;
+using std::pair;
+using std::make_pair;
 
 /*
 typedef vector<pair<string, date>> vtstr;
@@ -24,7 +29,7 @@ typedef vector<pair<date, date>> vtdt;
 typedef boost::variant<vtstr, vtint, vtdbl, vtdt> field_history_t;
 */
 
-typedef boost::variant<int, double, string, date> field_t;
+typedef boost::variant<bool, int, double, string, date> field_t;
 typedef pair<field_t, date> field_tp_t;
 typedef vector<field_tp_t> field_history_t;
 typedef unordered_map<string, field_history_t> fields_t;
@@ -32,35 +37,38 @@ typedef unordered_map<string, field_history_t> fields_t;
 class fieldsbase
 {
 public:
-    fieldsbase() {}
+    template <class T>
+    void insfieldhistory(const string &fname, T var, date dt) {
+        fields[fname].push_back(make_pair(var, dt));
+    }
+
     fields_t &getfields() {
         return fields;
     }
-    vector<string> fieldsname;
+    const vector<string> fieldsname;
 
 protected:
+    fieldsbase() {}     // 只能继承使用
     fields_t fields;
 };
 
+/*
 template <class T>
 void insfieldhistory(fieldsbase &f, string fname, T var, date dt) {
     f.getfields()[fname].push_back(make_pair(var, dt));
     return;
 }
-
-//const char *shipinfo_fieldname[] = {
-//const vector<string> ship_fn = {
-//        "name",
-//        "zd",
-//        "zzd",
-//        "gl"
-//};
+*/
 
 class shipinfo : public fieldsbase
 {
 public:
-    vector<string> fieldsname = {
-            "name", "zd", "zzd", "gl"
+    const vector<string> fieldsname = {
+            "id", "coid", "name", "cjg", "yyzbh", "yyzfzrq", "yyzyxq", "yyzarid",
+            "goyyzbh", "goyyzfzrq", "goyyzyxq", "goyyzarid", "stid", "cbjcrq",
+            "cbzd", "cbzzd", "cbteu", "cblfm", "cbcw", "cbkw", "cbgl", "cbjyfw",
+            "yngo", "ynjnm", "ynsj", "ynsn", "ynssj", "ynssn", "syr", "glr", "yntg",
+            "bz", "lsarid"
     };
     shipinfo() {
         // init fields' name
